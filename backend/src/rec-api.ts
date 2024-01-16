efebc056309711ee7b5fa5cbb46d141bb80332c0
Saksham <sakshamkohli6@gmail.com>
2024-01-16 02:24:00 -0500

    updated summary modal

--- backend/src/rec-api.ts
+++ backend/src/rec-api.ts
@@ -23,3 +23,41 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
     console.log(error);
   }
 };
+
+export const getRecipeSummary = async (recipeId: string) => {
+  if (!apiKey) {
+    throw new Error("API Key not found.");
+  }
+  const url = new URL(
+    `https://api.spoonacular.com/recipes/${recipeId}/summary`
+  );
+  const params = {
+    apiKey: apiKey,
+  };
+  url.search = new URLSearchParams(params).toString();
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+// export const getRandom = async (page: number) => {
+//   if (!apiKey) {
+//     throw new Error("API Key not found.");
+//   }
+
+//   const url = new URL("https://api.spoonacular.com/recipes/random");
+
+//   const Para = {
+//     apiKey,
+//     number: "10",
+//     offset: (page * 10).toString(),
+//   };
+//   url.search = new URLSearchParams(Para).toString();
+
+//   try {
+//     const searchRes = await fetch(url);
+//     const resultJson = await searchRes.json();
+//     return resultJson;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
